a03ca6ed0f5ad9dc28b6d4b081ab681c3b27ae51
novi2904 <99081480@mydavinci.nl>
2024-09-02 14:27:56 +0200

    tijd berkenen

---
+++ bc_l2_opdracht3 (2).py
@@ -0,0 +1,11 @@
+# We verwachten: 199.5126050420168
+print (((12*13 /14) + (15 -16) /17) *18 ) 
+
+# Maak met de volgende reeks 5 verschillende uitkomsten,
+# door haakjes op andere plekken te zetten
+print( 2*3 /4 + (5 -6/7) *-1)
+print( 2*5 /6 + (7 -8/6) *-2 )
+print( 4*7 /9 + (8 -6/3) *-7 )
+print( 8*9 /7 + (6 -5/2) *-9 )
+print( 3*9 /3 + (8 -3/1) *-2 )
+print( 4*3 /8 + (3 -4/2) *-3 )
---
+++ bc_l3_doemee.py
@@ -0,0 +1,16 @@
+# Wat gaat er gebeuren?
+getal1 = 25
+getal2 = 4
+print(getal1/getal2)
+
+naam = input("wat is je naam: ")
+
+# we gaan hier een variable inbouwen
+print(f'Hallo {naam}, ik leer nu programmeren.')
+
+woord = input("geef een woord: ")
+aantal_tekens = len(woord)
+
+# we schrijven code dat de gebruiker vraagt om een woord in te voeren
+# en vervolgens de volgende zin print:
+print (f"Het woord is: {woord} en bestaat uit {aantal_tekens} tekens.") 
