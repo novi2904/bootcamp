08753421c725151ab052404d666887989313cb2d
novi2904 <99081480@mydavinci.nl>
2024-09-04 09:12:29 +0200

    uuu

---
+++ bc_l4_doemee.py
@@ -0,0 +1,36 @@
+# we bekijken de volgende code en wat er gebeurd
+# we lossen misschien wat problemen op
+var1 = input('gebruikersnaam: ')
+var2 = input('wachtwoord: ')
+
+# print('')
+# print(f'{var1} == {var2}:', var1 == var2) 
+# print(f'{var1} != {var2}:', var1 != var2) 
+
+# print('')
+# print(f'{var1} > {var2}:', var1 > var2)
+# print(f'{var1} >= {var2}:', var1 >= var2)
+
+# print('')
+# print(f'{var1} <= {var2}:', var1 <= var2)
+# print(f'{var1} < {var2}:', var1 < var2)
+
+# We bekijken de volgende foute code en lossen de problemen op
+# en maken het netter
+# favoriteColor = input('En wat is je favoriete kleur? ') 
+# trueOrFalse = str(random.randint(0,1))
+# if trueOrFalse = True:
+# print('Ik vind dat ook een mooie kleur!')
+# elif trueOrFalse = False:
+# print('TBH, ik hou niet zo van {}...'.format(favoritecolor))
+
+if (var1 == "novi"):
+    print("correct gebruikersnaam!!")
+else:
+    print("incorrect gebruikersnaam!")
+
+if (var2 == "hond"):
+    print("correct wachtwoord!!")
+else:
+    print("incorrect wachtwoord!")
+    
---
+++ variabelen.py
@@ -0,0 +1,7 @@
+var1=("50")
+var2=("60")
+var3=("90")
+
+gem=((var1 + var2 + var3)/var4) 
+
+var4 = 3
